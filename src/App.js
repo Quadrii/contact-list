f91371e8808baf2b45e455d5239464b78f8a51a9
quadri <quadri@adisaquadri234@yahoo.com>
2020-06-03 00:53:53 +0100

    I learn how to use props in react, I also learn how to use state and I also fetch costumer's contact data from an API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import Contact1 from './Contact1';
 function App() {
+  // const contacts = [
+  //   {
+  //     avatarUrl: "https://via.placeholder.com/150",
+  //     name: "Jenny Han",
+  //     email: "jenny.han@notreal.com",
+  //     age: 25
+  //   },
+  //   {
+  //     avatarUrl: "https://via.placeholder.com/150",
+  //     name: "Paul Jones",
+  //     email: "paul.jones@notreal.com",
+  //     age: 33
+  //   },
+  //   {
+  //     avatarUrl: "https://via.placeholder.com/150",
+  //     name: "Peter Pan",
+  //     email: "peterpan@neverland.com",
+  //     age: 100
+  //   }
+  // ]
+
+  const  [results, setResults] = useState([]);
+  useEffect(()=> {
+    fetch ("https://randomuser.me/api/?results=5")
+    .then(response => response.json())
+    .then(data => {
+    console.log(data)
+    setResults(data.results)
+   });
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {results.map((result, index) => {
+        return (
+          <Contact1 
+          key = {index}
+          avatarUrl={result.picture.large}
+          name={result.name.first}
+          email={result.email}
+          age={result.dob.age}/>
+        )
+      })}
+
+      {/* <Contact1 
+        avatarUrl="https://via.placeholder.com/150"
+        name="Jenny Han"
+        email="jenny.han@notreal.com"
+        age={25}/>
+      <Contact1 
+        avatarUrl="https://via.placeholder.com/150"
+        name="Paul Jones"
+        email="paul.jones@notreal.com"
+        age={55}/>
+      <Contact1 
+        avatarUrl="https://via.placeholder.com/150"
+        name="Peter Pan"
+        email="peter.pan@neverland.com"
+        age={100}/> */}
     </div>
+    
   );
 }
 
